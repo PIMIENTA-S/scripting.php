c058f8403ac2641979c91e3ea8151b0ba67a4be1
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-11-29 00:29:16 -0500

    feat(arrays)

---
+++ Arreglos/asociativo.php
@@ -0,0 +1,15 @@
+<?php
+    // CREACION DE ARREGLOS
+    // Los arreglos asociativos son como los diccionarios en python
+
+    $lenguajes = ["lenguaje1" => 'PHP', "lenguaje2" => 'JavaScript', 
+    "lenguaje3" => 'C++'];
+
+    $skills = array("lenguaje1" => 'PHP', "lenguaje2" => 'JavaScript',
+     "lenguaje3" => 'C++');
+
+    // ACCEDER A LOS ELEMENTOS
+     echo $skills["lenguaje3"]
+
+    // IMPRIMIR ARREGLOS
+?>
---
+++ Arreglos/indexado.php
@@ -0,0 +1,12 @@
+<?php
+    // CREAR ARREGLOS
+
+    $numeros = [1,2,3,4,5];
+    $sucesiones = array(6,7,8,9,0);
+
+    echo $numeros[3]. "<br/>";
+
+    // IMPRIMIR ARREGLOS
+    var_dump($numeros);
+    print_r($sucesiones);
+?>
