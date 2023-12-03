349b04a54acc2418d19d127a23d53668eccf394a
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-12-03 14:47:06 -0500

    feat(index)

---
+++ practica/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>P.A.P.A</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Calcular PAPA</h1>
+
+    <main>
+        <div class="materia">
+            <h3>Materia</h3>
+            <form action="">
+                <input type="text" placeholder="Nombre Materia">
+            </form>
+        </div>
+
+        <div class="notas">
+            <h3>Nota</h3>
+            <form action="">
+                <input type="text" placeholder="Nota">
+            </form>
+        </div>
+
+        <div class="creditos">
+            <h3>Creditos</h3>
+            <form action="">
+                <select name="creditos" id="">
+                    <option value="">2</option>
+                    <option value="">3</option>
+                    <option value="">4</option>
+                    <option value="">5</option>
+                    <option value="">6</option>
+                </select>
+            </form>
+        </div>
+    </main>
+    <a href="#" class="añadir">+</a>
+
+
+
+    
+</body>
+</html>
