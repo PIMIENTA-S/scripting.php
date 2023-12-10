1fb578576e4f01a69823afdcf4c629546e796837
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-12-09 22:18:34 -0500

    routes

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Uid\NilUlid;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('laravel', function(){
+    return "primeros pasos con laravel";
+});
+
+
+
+Route::get('laravel/{link1}/{link2?}', function($link1, $link2=null){
+    
+    if ($link2==null){
+        return "que pasa valemia";
+    }else {
+        return "Esta es un prueba de los enrutadores php con direccion $link1 en la categoria: $link2";
+        
+    }
+    
+});
+Route::get('laravel/{link}', function($link){
+    return "bienvenido a: $link";
+});
+
+Route::get('laravel/{link1}/{link2}', function($link1, $link2){
+    return "Esta es un prueba de los enrutadores php con direccion $link1 en la categoria $link2";
 });
