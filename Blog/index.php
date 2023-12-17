856507db5959f2f11b3bf4525463fe7124892664
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-12-17 17:12:04 -0500

    feat(add structure)

---
+++ Blog/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <div class="center">
+        <div class="title">
+            <p class="texto">Inicio de sesion<p>
+        </div>
+        <div class="cart">
+            <div class="user pad">
+                <p>Usuario</p>
+                <input type="text" placeholder="Usuario">
+            </div>
+            <div class="pass pad">
+                <p>Contraseña</p>
+                <input type="password" placeholder="Contraseña">
+            </div>
+            <button type="submit">Iniciar sesion</button>
+        </div>
+    </div>
+</body>
+</html>
