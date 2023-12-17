a3716b26b6ffb9e7a6488fec842022aa8eec7c54
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-12-17 18:47:52 -0500

    fix(includes)

---
+++ Blog/plantillas/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../styles/home.css">
+</head>
+<body>
+    <header class="header">
+        <nav>
+            <ul class="nav-list">
+                <li><a href="index.php" class="actual">Inicio</a></li>
+                <li><a href="vista_alumnos.php">Alumnos</a></li>
+                <li><a href="vista_cursos.php">Cursos</a></li>
+                <li><a href="#">Cerrar sesión</a></li>
+            </ul>
+        </nav>
+    </header>
+    
