19fe7bb758404b7f018ca5a89986a5d3fe1e9209
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-12-19 21:50:36 -0500

    fix(bug cursos.css)

--- Blog/secciones/vista_cursos.php
+++ Blog/secciones/vista_cursos.php
@@ -1,6 +1,6 @@
 <?php include('../plantillas/links.php')?>
 
-<link rel="stylesheet" href="../styles/cursos.css">
+<link rel="stylesheet" href="../styles/vista_curso.css">
 
 <?php include('../plantillas/header.php')?>
 
@@ -21,15 +21,31 @@
             </section>
         </div>
         <div class="botones">
-            <button type="button" class="">Agregar</button>
-            <button type="button">Editar</button>
-            <button type="button">Borrar</button>
+            <button type="button" class="green">Agregar</button>
+            <button type="button" class="yellow">Editar</button>
+            <button type="button" class="red">Borrar</button>
         </div>
     </div>
 
 
     <div class="formulario">
-        <p>Formulario</p>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td> 1 </td>
+                    <td> Sitio web con PHP </td>
+                    <td> Seleccionar </td>
+                </tr>
+            </tbody>
+        </table>
+
     </div>
 </div>
 
